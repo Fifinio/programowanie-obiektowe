cb436a2d247b48891fdce3f1ac3aab4be1318d91
Fifinio <fiifnio13@gmail.com>
2022-03-02 09:47:19 +0100

    Lab#01

--- Lab#01/Program.cs
+++ Lab#01/Program.cs
@@ -8,26 +8,140 @@ namespace Lab_01
         {
             Console.WriteLine("Hello World!");
 
-            Person person = new Person("John", "Doe", 30);
-            Console.WriteLine(person);
+            PersonProperties person = new PersonProperties();
+            Console.WriteLine(person.firstName);
             
-        }
-        class Person
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public int Age { get; set; }
-        public Person(string firstName, string lastName, int age)
-        {
-            FirstName = firstName;
-            LastName = lastName;
-            Age = age;
-        }
-        
-        public override string ToString()
-        {
-            return $"{FirstName} {LastName} is {Age} years old.";
+            Vehicle vehicle = new Vehicle("Honda", "Civic", "Red", "2015", "123456789");
+            Console.WriteLine(vehicle);
         }
     }
+    class PersonProperties
+    {
+        private string FirstName;
+        public string firstName
+        {
+            get { return FirstName; }
+            set { 
+                if(value.Length >= 2){
+                    FirstName = value;
+                } 
+                }
+        }
+    }
+
+    class Vehicle {
+            public readonly string[] makes = {
+            "Abarth",
+            "Alfa Romeo",
+            "Aston Martin",
+            "Audi",
+            "Bentley",
+            "BMW",
+            "Bugatti",
+            "Cadillac",
+            "Chevrolet",
+            "Chrysler",
+            "Citroën",
+            "Dacia",
+            "Daewoo",
+            "Daihatsu",
+            "Dodge",
+            "Donkervoort",
+            "DS",
+            "Ferrari",
+            "Fiat",
+            "Fisker",
+            "Ford",
+            "Honda",
+            "Hummer",
+            "Hyundai",
+            "Infiniti",
+            "Iveco",
+            "Jaguar",
+            "Jeep",
+            "Kia",
+            "KTM",
+            "Lada",
+            "Lamborghini",
+            "Lancia",
+            "Land Rover",
+            "Landwind",
+            "Lexus",
+            "Lotus",
+            "Maserati",
+            "Maybach",
+            "Mazda",
+            "McLaren",
+            "Mercedes-Benz",
+            "MG",
+            "Mini",
+            "Mitsubishi",
+            "Morgan",
+            "Nissan",
+            "Opel",
+            "Peugeot",
+            "Porsche",
+            "Renault",
+            "Rolls-Royce",
+            "Rover",
+            "Saab",
+            "Seat",
+            "Skoda",
+            "Smart",
+            "SsangYong",
+            "Subaru",
+            "Suzuki",
+            "Tesla",
+            "Toyota",
+            "Volkswagen",
+            "Volvo"};
+        private string _Make;
+        private string _Model;
+        private int _Year;
+        private string _Color;
+        private string _LicensePlate;
+
+        public override string ToString()
+        {
+            return "Make: " + _Make + "\nModel: " + _Model + "\nYear: " + _Year + "\nColor: " + _Color + "\nLicense Plate: " + _LicensePlate;
+        }
+        private Vehicle(string make, string model, int year, string color, string licensePlate) {
+            _Make = make;
+            _Model = model;
+            _Year = year;
+            _Color = color;
+            _LicensePlate = licensePlate;
+        }
+
+
+        public static Vehicle Of(string make, string model, int year, string color, string licensePlate) {
+            Vehicle x = new Vehicle("Honda", "Civic", 2020, "black","testing123");
+            if( Array.IndexOf(x.makes, make) >= 0){
+                if(model.Length >= 2){
+                    if(year >= 1900 && year <= 2020){
+                        if(color.Length >= 2){
+                            if(licensePlate.Length >= 2){
+                                return new Vehicle(make, model, year, color, licensePlate);
+                            }
+                            else{
+                                throw new Exception("License plate must be at least 2 characters long");
+                            }
+                        }
+                        else{
+                            throw new Exception("Color must be at least 2 characters long");
+                        }
+                    }
+                    else{
+                        throw new Exception("Year must be between 1900 and 2020");
+                    }
+                }
+                else{
+                    throw new Exception("Model must be at least 2 characters long");
+                }
+            }
+            else{
+                throw new Exception("Make is not valid");       
+            } 
+        }
     }
 }
