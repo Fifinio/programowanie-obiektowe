c82afe49c530d469284d863bcf17fc068257b2ad
Fifinio <fiifnio13@gmail.com>
2022-03-02 08:29:08 +0100

    init on Lab#01

---
+++ Lab#01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Person person = new Person("John", "Doe", 30);
+            Console.WriteLine(person);
+        }
+    }
+}
