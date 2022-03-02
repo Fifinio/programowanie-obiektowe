5abd96ff8b3bae3d522d96e442e8c96c68eccc83
Fifinio <fiifnio13@gmail.com>
2022-03-02 08:35:38 +0100

    move all classes into programo.cs

--- Lab#01/Program.cs
+++ Lab#01/Program.cs
@@ -10,6 +10,24 @@ namespace Lab_01
 
             Person person = new Person("John", "Doe", 30);
             Console.WriteLine(person);
+            
         }
+        class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+        
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} is {Age} years old.";
+        }
+    }
     }
 }
