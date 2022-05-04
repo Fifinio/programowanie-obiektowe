2b4ee548998de4fcee0c64403ee1da114c3903b4
Fifinio <fiifnio13@gmail.com>
2022-05-04 08:54:57 +0200

    Break db

---
+++ Lab#09/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+static void Main(string[] args)
+{
+    System.Console.WriteLine("Hello World!");
+
+    DatabaseContext context = new DatabaseContext();
+    context.Database.EnsureCreated();
+    context.Students.Add(new Student { Name = "John", Age = 20 });
+    context.SaveChanges();
+
+
+}
+
+public class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
+public class DatabaseContext : DbContext
+{
+    // onconfigure
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlite("Data Source=Database.db");
+    }
+    // onmodelcreating
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>().ToTable("students");
+    }
+
+}
