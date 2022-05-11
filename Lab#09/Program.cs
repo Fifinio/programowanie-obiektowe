f72b651c515de55142ae96fc6d23b5f3cc61fbff
Fifinio <fiifnio13@gmail.com>
2022-05-11 09:08:23 +0200

    exam

--- Lab#09/Program.cs
+++ Lab#09/Program.cs
@@ -1,16 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
-static void Main(string[] args)
-{
-    System.Console.WriteLine("Hello World!");
 
-    DatabaseContext context = new DatabaseContext();
-    context.Database.EnsureCreated();
-    context.Students.Add(new Student { Name = "John", Age = 20 });
-    context.SaveChanges();
+System.Console.WriteLine("Hello World!");
+
+DatabaseContext context = new DatabaseContext();
+context.Database.EnsureCreated();
+// context.Students.Add(student);
+// context.Students.Add(new Student { Name = "John", Age = 20 });
+context.SaveChanges();
 
 
-}
 
 public class Student
 {
@@ -22,13 +21,14 @@ public class DatabaseContext : DbContext
     // onconfigure
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
+        // path to datasource is E://Database/Database.db
         base.OnConfiguring(optionsBuilder);
-        optionsBuilder.UseSqlite("Data Source=Database.db");
+        optionsBuilder.UseSqlite("DATASOURCE=Database.db");
     }
     // onmodelcreating
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Student>().ToTable("students");
+        modelBuilder.Entity<Student>().ToTable("Students");
     }
 
 }
---
+++ test/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Program
+{
+    record Car(string Model, int Power);
+    public static void Main(string[] args)
+    {
+        List<Car> cars = new List<Car>()
+        {
+            new Car("Fiat 500", 90),
+            new Car("Ford Kuga", 100),
+            new Car("Toyota Yaris", 90),
+            new Car("Audi A4", 200)
+        };
+        Car car = cars.Skip(3).ElementAt(1);
+        Console.WriteLine(car);
+    }
+}
